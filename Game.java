e25164003fabbf327adb128b0889f43631c02619
weakennN <77596296+weakennN@users.noreply.github.com>
2021-03-01 23:16:00 +0200

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,11 @@
+public class Game {
+
+    private Board board;
+    private Player[] players;
+
+    public Game(Board board, Player[] players) {
+        
+        this.board = board;
+        this.players = players;
+    }
+}
