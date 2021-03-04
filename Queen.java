97f3d37c91e1a1a0481e588729d0ad7f2427f066
weakennN <77596296+weakennN@users.noreply.github.com>
2021-03-04 16:24:00 +0200

    Add files via upload

--- Queen.java
+++ Queen.java
@@ -17,10 +17,11 @@ public class Queen extends Figure {
 
         super.setRowPosition(rowPosition);
         super.setCowPosition(cowPosition);
+        super.emptyMoves();
     }
 
     @Override
-    public String draw(int row,String color){
+    public String draw(int row, String color) {
 
         return color + figureDraw[row];
     }
@@ -28,15 +29,26 @@ public class Queen extends Figure {
     @Override
     public void possibleMoves() {
 
+        super.addPossibleRowPosition(super.getRowPosition() + 1);
+        super.addPossibleRowPosition(super.getRowPosition() - 1);
+        super.addPossibleColPosition(super.getColPosition() + 1);
+        super.addPossibleColPosition(super.getColPosition() - 1);
     }
 
     @Override
     protected void isMoveValid(int row, int col) {
 
+        if (super.getRowPosition() > row || super.getRowPosition() < row && super.getColPosition() != col && !super.getPossibleColPositions().contains(col)) {
+
+            throw new IllegalArgumentException("Invalid move,");
+        } else if (super.getColPosition() > col || super.getColPosition() < col && super.getRowPosition() != row && !super.getPossibleRowPositions().contains(row)) {
+            throw new IllegalArgumentException("Invalid move,");
+        }
     }
 
     @Override
     public void attackSquare(int row, int col) {
 
+        this.move(row, col);
     }
 }
